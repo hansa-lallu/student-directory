1b1d06e6d568467462d1fd202e254c37b64d6374
hansa-lallu <hansalallu@gmail.com>
2020-01-04 20:41:03 +0000

    step 5 - adding information

--- directory_copy.rb
+++ directory_copy.rb
@@ -1,16 +1,16 @@
 
 students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Coreleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november},
+  {name: "Dr. Hannibal Lecter", cohort: :november, country_of_birth: "Japan", height: 165},
+  {name: "Darth Vader", cohort: :november, country_of_birth: "New Zealand", height: 175},
+  {name: "Nurse Ratched", cohort: :november, country_of_birth: "Australia", height: 168},
+  {name: "Michael Coreleone", cohort: :november, country_of_birth: "Hawaii", height: 185},
+  {name: "Alex DeLarge", cohort: :november, country_of_birth: "UK", height: 178},
+  {name: "The Wicked Witch of the West", cohort: :november, country_of_birth: "UK", height: 163},
+  {name: "Terminator", cohort: :november, country_of_birth: "UK", height: 169},
+  {name: "Freddy Krueger", cohort: :november, country_of_birth: "Greece", height: 165},
+  {name: "The Joker", cohort: :november, country_of_birth: "Iceland", height: 178},
+  {name: "Joffrey Baratheon", cohort: :november, country_of_birth: "Turkey", height: 181},
+  {name: "Norman Bates", cohort: :november, country_of_birth: "Japan", height: 183},
 ]
 
 def input_students
@@ -39,7 +39,7 @@ end
 def print(students)
   count = 0
   while count < students.length do 
-    puts "#{count + 1}. #{students[count][:name]} (#{students[count][:cohort]} cohort)"
+    puts "#{count + 1}. #{students[count][:name]} (#{students[count][:cohort]} cohort), #{students[count][:country_of_birth]}, #{students[count][:height]}."
     count = count + 1
   end 
   # students.each.with_index(1) do |student, index|
