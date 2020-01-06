dfb95eed6759a9add949b83190c18e93ebf633cd
hansa-lallu <hansalallu@gmail.com>
2020-01-06 21:59:56 +0000

    Step 12

--- directory_copy.rb
+++ directory_copy.rb
@@ -55,6 +55,7 @@ def print_menu
     puts "1. Input the students"
     puts "2. Show the students"
     puts "3. Save the list to students.csv"
+    puts "4. Load the list from students.csv"
     puts "9. Exit"
 end 
 
@@ -72,6 +73,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit
   else 
@@ -83,5 +86,15 @@ def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end 
+  file.close
+end 
+
 
 interactive_menu
