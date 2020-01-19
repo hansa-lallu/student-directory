032703689c94730d7430fa656572b244e13d4e79
hansa-lallu <hansalallu@gmail.com>
2020-01-19 18:38:25 +0000

    step 14 - ex 2

--- directory_copy.rb
+++ directory_copy.rb
@@ -100,11 +100,13 @@ def save_students
   file.close 
 end 
 
-
-
 def try_load_students
   filename = ARGV.first # first argument from the command line
-  return if filename.nil? # get out of the method if it isn't given
+    
+  if filename.nil? # get out of the method if it isn't given
+    filename = 'students.csv'
+  end
+
   if File.exists?(filename) # if it exists
     load_students(filename)
      puts "Loaded #{@students.count} from #{filename}"
